16a6c57acdde3db36c283535afc84035bee35dfd
SimoneVilardo <simovila95@gmail.com>
2023-11-10 12:52:44 +0100

    snack4 completato

---
+++ src/org/lessons/java/Snack4.java
@@ -0,0 +1,33 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack4 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserisci una parola: ");
+		String strUser = in.nextLine();
+		
+		int strLength = strUser.length();		
+		String stringReverse = "";
+		
+		for (int i = strLength - 1; i >= 0; i--) {
+
+            char c = strUser.charAt(i);
+
+           stringReverse += c;
+
+        }
+		
+		if (strUser.equals(stringReverse)) {
+			System.out.println("La parola " + strUser + " è palindroma");
+		}else {
+			System.out.println("La parola non è palindroma");
+		}
+
+	}
+
+}
