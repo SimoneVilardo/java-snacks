334fab6d1760bdcaf1d42d085ee96d63fce54380
SimoneVilardo <simovila95@gmail.com>
2023-11-10 11:43:58 +0100

    snack1 completato

--- src/org/lessons/java/snack1.java
+++ src/org/lessons/java/snack1.java
@@ -1,9 +1,22 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class snack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		String strValue = in.nextLine();
+		int value = Integer.valueOf(strValue);
+		
+		if(value % 2 == 0) {
+			System.out.println(value);
+		}else {
+			System.out.println(value + 1);
+		}
 
 	}
 
