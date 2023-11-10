a619592bd78c4a914eabe36ab90369fb0681a83f
SimoneVilardo <simovila95@gmail.com>
2023-11-10 15:15:45 +0100

    snack7 completato

---
+++ src/org/lessons/java/Snack7.java
@@ -0,0 +1,25 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack7 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Inserisci i secondi: ");
+		String strSecondi= in.nextLine();
+		int intSecondi = Integer.valueOf(strSecondi);
+		
+		int ore = intSecondi / 3600;
+        int secondiRimanenti = intSecondi % 3600;
+        int minuti= secondiRimanenti / 60;
+        int secondi = secondiRimanenti % 60;
+
+        String strTempo = String.format("%02d:%02d:%02d", ore, minuti, secondi);
+
+        System.out.println("Il tempo convertito è: " + strTempo);
+	}
+
+}
